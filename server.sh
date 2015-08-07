9765ba0ed127949f5447e372c7220243c924cdcd
georgegeddes <georgegdds@gmail.com>
2015-08-08 00:10:04 +0200

    Update server.sh
    This way it works with both ikon and pixis.

--- server.sh
+++ server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd /home/pixis/hitandmis.server/
+cd $HOME/hitandmis.server/
 #nodemon app.js
 forever -a -l server.log -o server.log -e server.log start --uid "hitandmis.server" app.js
